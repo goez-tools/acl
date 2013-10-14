ca2594a71cba6dff84b08f721d9984c56f5a85e3
Jace Ju <jaceju@kkbox.com>
2013-10-14 20:27:26 +0800

    Complete 'allow', 'deny', 'can' methods.

--- src/Goez/Acl/Role.php
+++ src/Goez/Acl/Role.php
@@ -6,7 +6,10 @@ class Role
 {
     protected $_name = 'role';
 
-    protected $_permissions = array();
+    protected $_rules = array(
+        'allowed' => array(),
+        'denied' => array(),
+    );
 
     /**
      * @param $name
@@ -21,34 +24,86 @@ class Role
         $this->_name = $name;
     }
 
+    /**
+     * @param string $action
+     * @param mixed $resource
+     * @return \Goez\Acl\Role
+     * @throws \Goez\Acl\Exception
+     */
     public function allow($action, $resource)
     {
-        $resource = is_object($resource) ? get_class($resource) : $resource;
+        $this->_addRule('allowed', $action, $resource);
+
+        return $this;
+    }
+
+    /**
+     * @param string $action
+     * @param mixed $resource
+     * @return \Goez\Acl\Role
+     * @throws \Goez\Acl\Exception
+     */
+    public function deny($action, $resource)
+    {
+        $this->_addRule('denied', $action, $resource);
+
+        return $this;
+    }
+
+    /**
+     * @param string $type
+     * @param string $action
+     * @param mixed $resource
+     * @return \Goez\Acl\Role
+     * @throws \Goez\Acl\Exception
+     */
+    protected function _addRule($type, $action, $resource)
+    {
+        $resource = $this->_getResourceName($resource);
 
         if (!is_string($resource)) {
             throw new Exception('Resource must be string or object.');
         }
 
-        if (!isset($this->_permissions[$resource])) {
-            $this->_permissions[$resource] = array();
+        if (!isset($this->_rules[$type][$resource])) {
+            $this->_rules[$type][$resource] = array();
         }
 
         $action = strtolower($action);
-        $this->_permissions[$resource][] = $action;
-
-        return $this;
+        $this->_rules[$type][$resource][] = $action;
     }
 
-    public function deny($action, $resource)
+    /**
+     * @param string $action
+     * @param string $resource
+     * @return bool
+     */
+    public function can($action, $resource)
     {
+        $action = strtolower($action);
+        $resource = $this->_getResourceName($resource);
 
+        foreach (array('denied', 'allowed') as $type) {
+            $rules = $this->_rules[$type];
+            if (!isset($rules[$resource])) {
+                continue;
+            }
+            if (in_array($action, $rules[$resource])) {
+                return ($type === 'allowed');
+            }
+        }
 
-        return $this;
+        return false;
     }
 
-    public function can($action, $resource, $resourceValue = null)
+    /**
+     * @param mixed $resource
+     * @return string
+     */
+    protected function _getResourceName($resource)
     {
-        return true;
+        return is_object($resource) ? get_class($resource) : $resource;
     }
+
 
 }
--- tests/GoezTest/Acl/AclTest.php
+++ tests/GoezTest/Acl/AclTest.php
@@ -26,4 +26,33 @@ class AclTest extends \PHPUnit_Framework_TestCase
 
     }
 
+    public function testPermissionForReadOnly()
+    {
+        $guest = $this->_acl->addRole('guest')->getRole('guest');
+
+        $guest->allow('read', 'article');
+        $guest->deny('write', 'article');
+
+        $this->assertTrue($guest->can('read', 'article'));
+        $this->assertFalse($guest->can('write', 'article'));
+    }
+
+    public function testPermissionForReadAndRewrite()
+    {
+        $author = $this->_acl->addRole('author')->getRole('author');
+
+        $author->allow('read', 'article');
+        $author->allow('write', 'article');
+        $author->deny('create', 'page');
+        $author->deny('create', 'site');
+
+
+        $this->assertFalse($author->can('create', 'page'));
+        $this->assertFalse($author->can('create', 'site'));
+
+        $this->assertTrue($author->can('read', 'article'));
+        $this->assertTrue($author->can('write', 'article'));
+
+    }
+
 }
