ef3de518e72f95bac40ff208298bcf02f9be39eb
Jace Ju <jaceju@kkbox.com>
2013-10-17 12:01:04 +0800

    Add config example of ACL.

---
+++ src/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    'init' => function ($acl) {
+
+        // Initialize your permission here.
+        // Example:
+        //
+        // $acl->fullPrivileges('admin');
+        // $acl->allow('author', 'read', 'article');
+        // $acl->allow('author', 'write', 'article');
+        // $acl->allow('guest', 'read', 'article');
+        // $acl->deny('guest', 'write', 'article');
+
+    },
+);
